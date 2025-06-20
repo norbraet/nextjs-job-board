1f943b076f4960ea7bf8f0d29432b5da9db6996d
Norbert Balaz <n.balaz@s-v.de>
2025-06-20 12:08:19 +0200

    feat: add initial migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { env } from "@/data/env/server"
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-    out: "./src/drizzle",
+    out: "./src/drizzle/migrations",
     schema: "./src/drizzle/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
