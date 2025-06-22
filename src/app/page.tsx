83f29a9f16541db13bcc503f3d12172ac57c0998
Norbert Balaz <n.balaz@s-v.de>
2025-06-22 15:54:39 +0200

    feat: integrate AppSidebarClient for mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,33 @@
 import { Sidebar, SidebarHeader, SidebarProvider, SidebarTrigger, SidebarContent, SidebarFooter, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar"
+import { AppSidebarClient } from "@/app/_AppSidebarClient"
 
 export default function HomePage() {
   return (
     <SidebarProvider className="overflow-y-hidden">
-      <Sidebar collapsible="icon" className="overflow-hidden">
-        <SidebarHeader className="flex-row">
-          <SidebarTrigger />
-          <span className="text-xl text-nowrap">Job Portal</span>
-        </SidebarHeader>
-        <SidebarContent>
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+            <span className="text-xl text-nowrap">Job Portal</span>
+          </SidebarHeader>
+          <SidebarContent>
 
-        </SidebarContent>
-        <SidebarFooter>
-          <SidebarMenu>
-            <SidebarMenuItem>
-              <SidebarMenuButton>
-                asd
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          </SidebarMenu>
-        </SidebarFooter>
-      </Sidebar>
-      <main className="flex-1">
-        asdadfa
-      </main>
+          </SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>
+                  asd
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">
+          asdadfa
+        </main>
+      </AppSidebarClient>
+  
     </SidebarProvider>
   )
 }
