25895f3ec338b3d41b75a88a0abc05e78d693dfa
Norbert Balaz <n.balaz@s-v.de>
2025-07-14 10:38:34 +0200

    feat: add Clerk webhook integration and user creation functionality

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,10 +1,11 @@
 import { inngest } from "@/services/inngest/client"
+import { clerkCreateUser } from "@/services/inngest/functions/clerk"
 import { serve } from "inngest/next"
 
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
     functions: [
-
+        clerkCreateUser
     ],
 })
